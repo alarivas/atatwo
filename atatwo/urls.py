5d09ed51eeeabe0ad6c6bb11257fefa2dbda6032
hnromante <hugonunez@thedogcompany.cl>
2018-07-08 01:35:13 -0400

    Imagenes /MEDIA

--- atatwo/urls.py
+++ atatwo/urls.py
@@ -17,9 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
 from front import views as front_views
+from django.conf.urls import url, include
+from django.conf import settings
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notifications.urls')),
     path('', front_views.index)
 ]
+
+if settings.DEBUG:
+    urlpatterns +=  [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
