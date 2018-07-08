2184a884b7c226dd75fb8bd2d088b7996452ff36
hnromante <hugonunez@thedogcompany.cl>
2018-07-08 11:52:15 -0400

    cambios

--- atatwo/settings.py
+++ atatwo/settings.py
@@ -141,7 +141,6 @@ DATABASES['default'].update(db_from_env)
 #STATIC
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
